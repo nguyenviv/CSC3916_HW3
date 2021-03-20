e2c33e623ebf0716b4ad9a4c3467d4580fbec15a
nguyenviv <72362744+nguyenviv@users.noreply.github.com>
2021-03-19 22:59:12 -0600

    Update PUT

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ router.route('/movies')
                    movie.genre = req.body.genre;
                    movie.actors = req.body.actors;
 
-                   movie.save(function (err) {
+                   movie.save(function (err, movies) {
                        if (err) throw err;
                        //else
                        //console.log(movies);
